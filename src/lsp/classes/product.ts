7da8e944d8316ba51645ce3d72f462d331c5ba52
Gustavo Kurek <gustavokureksz@gmail.com>
2022-07-20 17:45:22 -0300

    second commit

--- src/lsp/classes/product.ts
+++
@@ -1,5 +0,0 @@
-import { Items } from './interfaces/card-items';
-
-export class Product implements Items {
-  constructor(public name: string, public price: number) {}
-}
--- src/ocp/classes/interfaces/card-items.ts
+++
@@ -1,1 +0,0 @@
-export type Items = { name: string; price: number };
